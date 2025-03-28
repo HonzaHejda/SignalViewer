a1425adff14bafc9ddc57d249f912c1294708422
HonzaHejda <88653738+HonzaHejda@users.noreply.github.com>
2025-03-28 11:06:01 +0100

    Signal envelope
    Mac update

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import plotly.subplots as sp
 import tkinter as tk
 from PyQt6.QtWidgets import QApplication, QFileDialog
 import plotly.io as pio
+import neurokit2 as nk
+from scipy.signal import butter, filtfilt
 
 # Funkce pro načtení více souborů
 def load_csv_files():
@@ -13,6 +15,14 @@ def load_csv_files():
         None, "Select Files", "", "CSV/TXT Files (*.csv *.txt)"
     )
     return file_paths
+
+
+def custom_highpass(signal, cutoff=10, fs=200, order=4):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    filtered = filtfilt(b, a, signal)
+    return filtered
 
 
 # Načtení datových souborů
@@ -30,7 +40,12 @@ fig = sp.make_subplots(rows=num_files, cols=1, shared_xaxes=True, vertical_spaci
 for i, file_path in enumerate(file_paths):
     df = pd.read_csv(file_path, sep="\t", header=None, names=["Sample", "Signal"], dtype={"Sample": int, "Signal": float})
     df["Time"] = df["Sample"] / 200  # Přepočet čísla vzorku na čas
-    fig.add_trace(go.Scatter(x=df["Time"], y=df["Signal"], mode='lines', name=f"Signal {i+1}"), row=i+1, col=1)
+
+    # Výpočet obálky signálu pomocí neurokit2
+    cleaned = custom_highpass(df["Signal"], cutoff=10, fs=200)
+    amplitude = nk.emg_amplitude(cleaned)
+
+    fig.add_trace(go.Scatter(x=df["Time"], y=amplitude, mode='lines', name=f"Signal {i+1}"), row=i+1, col=1)
 
 # Nastavení layoutu
 fig.update_layout(
